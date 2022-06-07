58a0c02335a3d6ee444aff9a86a6521359e6df12
vfegger <vitor@psr-inc.com>
2022-06-07 00:43:31 -0300

    Minor Update

--- recompile.sh
+++ recompile.sh
@@ -16,12 +16,12 @@ MEMORY_CHECK_OPTIONS="--log-file=$MEMORY_CHECK_FILENAME"
 
 if [ "$DEBUG" = "-d" ] || [ "$DEBUG" = "-debug" ] || [ "$DEBUG" = "-DEBUG" ] || [ "$DEBUG" = "-Debug" ];
 then
-    echo Debug Mode
+    echo "Debug Mode"
     BUILD_PATH=$DIR_PATH/$BUILD_DIR"-debug"
     BUILD_OPTIONS+="-DCMAKE_BUILD_TYPE=Debug"
     MEMORY_CHECK_OPTIONS+=" --leak-check=full --track-origins=yes"
 else
-    echo Normal Mode
+    echo "Normal Mode"
     BUILD_PATH=$DIR_PATH/$BUILD_DIR
 fi
 rm $DATA_PATH/text/out/*.dat
@@ -32,3 +32,16 @@ cmake -S $SOURCE_PATH -B $BUILD_PATH $BUILD_OPTIONS
 cmake --build $BUILD_PATH
 valgrind $MEMORY_CHECK_OPTIONS $BUILD_PATH/UKF_1
 $BUILD_PATH/UKF_1
+
+GRAPH_PATH=$DIR_PATH/graph
+
+echo "Running Parser for GNUPlot" 
+$BUILD_PATH/graph/GRAPH_UKF
+echo "Finished GNUPlot Parser Execution"
+
+echo "Running GNUPlot for graph generation"
+cd $GRAPH_PATH
+gnuplot gnuplot_script.txt
+echo "Finished GNUPlot Execution"
+
+echo "Finished Execution"
