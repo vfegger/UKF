d26e5d71d5586b6336012c22b178e7e8e7bca644
vfegger <vitor@psr-inc.com>
2022-08-31 07:36:17 -0300

    Minor Update

--- src/math/include/MathGPU.hpp
+++ src/math/include/MathGPU.hpp
@@ -5,74 +5,79 @@
 #include <iostream>
 #include <iomanip>
 
-namespace MathGPU {
-    
-    static cudaDeviceProp* properties;
+#include <cuda.h>
+#include <cublas_v2.h>
+#include <cusolverDn.h>
+
+namespace MathGPU
+{
+
+    static cudaDeviceProp *properties;
 
     void InitializeGPUContext(int device);
 
-    void PrintMatrix(double* matrix_in, unsigned lengthX_in, unsigned lengthY_in, unsigned precision = 5u);
+    void PrintMatrix(double *matrix_in, unsigned lengthX_in, unsigned lengthY_in, unsigned precision = 5u);
 
-    //Out-Placed Calculation
-    void Add(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in, cudaStream_t stream = cudaStreamDefault);
-    void Sub(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in, cudaStream_t stream = cudaStreamDefault);
-    void Mul(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in, cudaStream_t stream = cudaStreamDefault);
-    void Mul(double* vector_out, double* vectorLeft_in, double value_in, unsigned length_in, cudaStream_t stream = cudaStreamDefault);
+    // Out-Placed Calculation
+    void Add(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in, cudaStream_t stream = cudaStreamDefault);
+    void Sub(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in, cudaStream_t stream = cudaStreamDefault);
+    void Mul(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in, cudaStream_t stream = cudaStreamDefault);
+    void Mul(double *vector_out, double *vectorLeft_in, double value_in, unsigned length_in, cudaStream_t stream = cudaStreamDefault);
 
-    //In-Placed Calculation
-    void Add(double* vector_inout, double* vector_in, unsigned length_in, cudaStream_t stream);
-    void Sub(double* vector_inout, double* vector_in, unsigned length_in, cudaStream_t stream);
-    void Mul(double* vector_inout, double* vector_in, unsigned length_in, cudaStream_t stream);
-    void Mul(double* vector_inout, double value_in, unsigned length_in, cudaStream_t stream);
+    // In-Placed Calculation
+    void Add(double *vector_inout, double *vector_in, unsigned length_in, cudaStream_t stream);
+    void Sub(double *vector_inout, double *vector_in, unsigned length_in, cudaStream_t stream);
+    void Mul(double *vector_inout, double *vector_in, unsigned length_in, cudaStream_t stream);
+    void Mul(double *vector_inout, double value_in, unsigned length_in, cudaStream_t stream);
 
-    //Matrix Multiplication
-    void MatrixMultiplication(double* matrix_out, double alpha, double beta,
-    double* matrixLeft_in, MatrixStructure structureLeft, unsigned lengthLeftX_in, unsigned lengthLeftY_in,
-    double* matrixRight_in, MatrixStructure structureRight, unsigned lengthRightX_in, unsigned lengthRightY_in,
-    double* weight_in = NULL);
-    
-    //Operators
-    void Operation(void (*operation_in)(double* matrix_out, double* matrixLeft_in, double* matrixRight_in, unsigned length_in),
-    double* matrix_out, double* matrixLeft_in, double* matrixRight_in, unsigned length_in,
-    unsigned strideLeft_in, unsigned strideRight_in, unsigned iteration_in = 1u, unsigned offsetLeft_in = 0u, unsigned offsetRight_in = 0u);
-    void Operation(void (*operation_in)(double* matrixLeft_inout, double* matrixRight_in, unsigned length_in),
-    double* matrixLeft_inout, double* matrixRight_in, unsigned length_in,
-    unsigned strideLeft_in, unsigned strideRight_in, unsigned iteration_in = 1u, unsigned offsetLeft_in = 0u, unsigned offsetRight_in = 0u);
+    // Matrix Multiplication
+    void MatrixMultiplication(double *matrix_out, double alpha, double beta,
+                              double *matrixLeft_in, MatrixStructure structureLeft, unsigned lengthLeftX_in, unsigned lengthLeftY_in,
+                              double *matrixRight_in, MatrixStructure structureRight, unsigned lengthRightX_in, unsigned lengthRightY_in,
+                              double *weight_in = NULL, cudaStream_t stream = cudaStreamDefault);
 
-    //Reducibles Operations
-    double Mean(double* vector_in, unsigned length_in, double* weight_in = NULL);
-    void Mean(double* vector_out, double* matrix_in, unsigned lengthX_in, unsigned lengthY_in, double* weight_in = NULL);
+    // Operators
+    void Operation(void (*operation_in)(double *matrix_out, double *matrixLeft_in, double *matrixRight_in, unsigned length_in),
+                   double *matrix_out, double *matrixLeft_in, double *matrixRight_in, unsigned length_in,
+                   unsigned strideLeft_in, unsigned strideRight_in, unsigned iteration_in = 1u, unsigned offsetLeft_in = 0u, unsigned offsetRight_in = 0u);
+    void Operation(void (*operation_in)(double *matrixLeft_inout, double *matrixRight_in, unsigned length_in),
+                   double *matrixLeft_inout, double *matrixRight_in, unsigned length_in,
+                   unsigned strideLeft_in, unsigned strideRight_in, unsigned iteration_in = 1u, unsigned offsetLeft_in = 0u, unsigned offsetRight_in = 0u);
 
-    //Linear System Solvers
-    void ForwardSubstitution(double* X_out, double* A, double* B,
-    unsigned lengthAX_in, unsigned lengthAY_in,
-    unsigned lengthBX_in, unsigned lengthBY_in,
-    unsigned lengthXX_in, unsigned lengthXY_in);
-    void BackwardSubstitution(double* X_out, double* A, double* B,
-    unsigned lengthAX_in, unsigned lengthAY_in,
-    unsigned lengthBX_in, unsigned lengthBY_in,
-    unsigned lengthXX_in, unsigned lengthXY_in);
+    // Reducibles Operations
+    double Mean(double *vector_in, unsigned length_in, double *weight_in = NULL);
+    void Mean(double *vector_out, double *matrix_in, unsigned lengthX_in, unsigned lengthY_in, double *weight_in = NULL);
 
-    //Direct Methods
+    // Linear System Solvers
+    void ForwardSubstitution(double *X_out, double *A, double *B,
+                             unsigned lengthAX_in, unsigned lengthAY_in,
+                             unsigned lengthBX_in, unsigned lengthBY_in,
+                             unsigned lengthXX_in, unsigned lengthXY_in);
+    void BackwardSubstitution(double *X_out, double *A, double *B,
+                              unsigned lengthAX_in, unsigned lengthAY_in,
+                              unsigned lengthBX_in, unsigned lengthBY_in,
+                              unsigned lengthXX_in, unsigned lengthXY_in);
+
+    // Direct Methods
     void LU();
     void LUP();
     void QR();
-    
-    //Iterative Methods
+
+    // Iterative Methods
     void GJ();
     void GS();
 
-    //Wrapper Methods
-    void LHSolver(double* X_out, double* A, double* B,
-    unsigned lengthAX_in, unsigned lengthAY_in,
-    unsigned lengthBX_in, unsigned lengthBY_in,
-    unsigned lengthXX_in, unsigned lengthXY_in,
-    double tol = 0.0001);
-    void RHSolver(double* X_out, double* A, double* B,
-    unsigned lengthAX_in, unsigned lengthAY_in,
-    unsigned lengthBX_in, unsigned lengthBY_in,
-    unsigned lengthXX_in, unsigned lengthXY_in,
-    double tol = 0.0001);
+    // Wrapper Methods
+    void LHSolver(double *X_out, double *A, double *B,
+                  unsigned lengthAX_in, unsigned lengthAY_in,
+                  unsigned lengthBX_in, unsigned lengthBY_in,
+                  unsigned lengthXX_in, unsigned lengthXY_in,
+                  double tol = 0.0001);
+    void RHSolver(double *X_out, double *A, double *B,
+                  unsigned lengthAX_in, unsigned lengthAY_in,
+                  unsigned lengthBX_in, unsigned lengthBY_in,
+                  unsigned lengthXX_in, unsigned lengthXY_in,
+                  double tol = 0.0001);
 }
 
 #endif
--- src/math/src/MathGPU.cu
+++ src/math/src/MathGPU.cu
@@ -1,133 +1,240 @@
 #include "MathGPU.hpp"
 
-void InitializeGPUContext(int device){
+void MathGPU::InitializeGPUContext(int device)
+{
     cudaError_t status = cudaSetDevice(device);
-    if(status != cudaSuccess){
+    if (status != cudaSuccess)
+    {
         cudaGetDeviceProperties(MathGPU::properties, device);
-    } else {
+    }
+    else
+    {
         std::cout << "Cuda-enabled context was not able to launch properly with the chosen device.\n";
     }
 }
 
-void MathGPU::PrintMatrix(double* matrix_in, unsigned lengthX_in, unsigned lengthY_in, unsigned precision){
+void MathGPU::PrintMatrix(double *matrix_in, unsigned lengthX_in, unsigned lengthY_in, unsigned precision)
+{
     // Get values from GPU pointer to CPU pointer
-    double* matrix_aux = new double[lengthX_in*lengthY_in];
-    cudaMemcpy(matrix_aux,matrix_in,sizeof(double)*lengthX_in*lengthY_in,cudaMemcpyDeviceToHost);
+    double *matrix_aux = new double[lengthX_in * lengthY_in];
+    cudaMemcpy(matrix_aux, matrix_in, sizeof(double) * lengthX_in * lengthY_in, cudaMemcpyDeviceToHost);
 
     std::cout << "Sizes: X = " << lengthX_in << "; Y = " << lengthY_in << "\n";
     std::cout.precision(precision);
     std::cout << std::scientific;
-    for(unsigned i = 0u; i < lengthX_in; i++){
+    for (unsigned i = 0u; i < lengthX_in; i++)
+    {
         std::cout << "| ";
-        for(unsigned j = 0u; j < lengthY_in; j++){
-            std::cout << matrix_aux[j*lengthX_in+i];
+        for (unsigned j = 0u; j < lengthY_in; j++)
+        {
+            std::cout << matrix_aux[j * lengthX_in + i];
         }
-        std::cout <<" |\n";
+        std::cout << " |\n";
     }
 }
 
-//Out-Placed Calculation Device
+// Out-Placed Calculation Device
 
-__device__ void _Add(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in){
+__device__ void _Add(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in)
+{
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index < length_in){
+    if (index < length_in)
+    {
         vector_out[index] = vectorLeft_in[index] + vectorRight_in[index];
     }
 }
-__device__ void _Sub(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in){
+__device__ void _Sub(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in)
+{
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index < length_in){
+    if (index < length_in)
+    {
         vector_out[index] = vectorLeft_in[index] - vectorRight_in[index];
     }
 }
-__device__ void _Mul(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in){
+__device__ void _Mul(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in)
+{
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index < length_in){
+    if (index < length_in)
+    {
         vector_out[index] = vectorLeft_in[index] * vectorRight_in[index];
     }
 }
-__device__ void _Mul(double* vector_out, double* vectorLeft_in, double value_in, unsigned length_in){
+__device__ void _Mul(double *vector_out, double *vectorLeft_in, double value_in, unsigned length_in)
+{
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index < length_in){
+    if (index < length_in)
+    {
         vector_out[index] = vectorLeft_in[index] * value_in;
     }
 }
 
-//Out-Placed Calculation Host
-void Add(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in, cudaStream_t stream){
+// Out-Placed Calculation Host
+void MathGPU::Add(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in, cudaStream_t stream)
+{
     unsigned length = MathGPU::properties->maxThreadsPerBlock;
     dim3 T(length);
-    dim3 B((length_in+T.x-1u)/T.x);
-    _Add<<<T,B,0,stream>>>(vector_out, vectorLeft_in, vectorRight_in, length_in);
+    dim3 B((length_in + T.x - 1u) / T.x);
+    _Add<<<T, B, 0, stream>>>(vector_out, vectorLeft_in, vectorRight_in, length_in);
 }
-void Sub(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in, cudaStream_t stream){
-        unsigned length = MathGPU::properties->maxThreadsPerBlock;
-    dim3 T(length);
-    dim3 B((length_in+T.x-1u)/T.x);
-    _Sub<<<T,B,0,stream>>>(vector_out, vectorLeft_in, vectorRight_in, length_in);
-}
-void Mul(double* vector_out, double* vectorLeft_in, double* vectorRight_in, unsigned length_in, cudaStream_t stream){
-        unsigned length = MathGPU::properties->maxThreadsPerBlock;
-    dim3 T(length);
-    dim3 B((length_in+T.x-1u)/T.x);
-    _Mul<<<T,B,0,stream>>>(vector_out, vectorLeft_in, vectorRight_in, length_in);
-}
-void Mul(double* vector_out, double* vectorLeft_in, double value_in, unsigned length_in, cudaStream_t stream){
+void MathGPU::Sub(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in, cudaStream_t stream)
+{
     unsigned length = MathGPU::properties->maxThreadsPerBlock;
     dim3 T(length);
-    dim3 B((length_in+T.x-1u)/T.x);
-    _Mul<<<T,B,0,stream>>>(vector_out, vectorLeft_in, value_in, length_in);
+    dim3 B((length_in + T.x - 1u) / T.x);
+    _Sub<<<T, B, 0, stream>>>(vector_out, vectorLeft_in, vectorRight_in, length_in);
+}
+void MathGPU::Mul(double *vector_out, double *vectorLeft_in, double *vectorRight_in, unsigned length_in, cudaStream_t stream)
+{
+    unsigned length = MathGPU::properties->maxThreadsPerBlock;
+    dim3 T(length);
+    dim3 B((length_in + T.x - 1u) / T.x);
+    _Mul<<<T, B, 0, stream>>>(vector_out, vectorLeft_in, vectorRight_in, length_in);
+}
+void MathGPU::Mul(double *vector_out, double *vectorLeft_in, double value_in, unsigned length_in, cudaStream_t stream)
+{
+    unsigned length = MathGPU::properties->maxThreadsPerBlock;
+    dim3 T(length);
+    dim3 B((length_in + T.x - 1u) / T.x);
+    _Mul<<<T, B, 0, stream>>>(vector_out, vectorLeft_in, value_in, length_in);
 }
 
-
-//Out-Placed Calculation Device
-__device__ void _Add(double* vector_inout, double* vector_in, unsigned length_in){
+// Out-Placed Calculation Device
+__device__ void _Add(double *vector_inout, double *vector_in, unsigned length_in)
+{
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index < length_in){
+    if (index < length_in)
+    {
         vector_inout[index] += vector_in[index];
     }
 }
-__device__ void _Sub(double* vector_inout, double* vector_in, unsigned length_in){
+__device__ void _Sub(double *vector_inout, double *vector_in, unsigned length_in)
+{
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index < length_in){
+    if (index < length_in)
+    {
         vector_inout[index] -= vector_in[index];
     }
 }
-__device__ void _Mul(double* vector_inout, double* vector_in, unsigned length_in){
+__device__ void _Mul(double *vector_inout, double *vector_in, unsigned length_in)
+{
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index < length_in){
+    if (index < length_in)
+    {
         vector_inout[index] *= vector_in[index];
     }
 }
-__device__ void _Mul(double* vector_inout, double value_in, unsigned length_in){
+__device__ void _Mul(double *vector_inout, double value_in, unsigned length_in)
+{
     unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index < length_in){
+    if (index < length_in)
+    {
         vector_inout[index] *= value_in;
     }
 }
 
-//In-Placed Calculation
-void Add(double* vector_inout, double* vector_in, unsigned length_in, cudaStream_t stream) {
+// In-Placed Calculation
+void MathGPU::Add(double *vector_inout, double *vector_in, unsigned length_in, cudaStream_t stream)
+{
     unsigned length = MathGPU::properties->maxThreadsPerBlock;
     dim3 T(length);
-    dim3 B((length_in+T.x-1u)/T.x);
-    _Add<<<T,B,0,stream>>>(vector_inout, vector_in, length_in);
+    dim3 B((length_in + T.x - 1u) / T.x);
+    _Add<<<T, B, 0, stream>>>(vector_inout, vector_in, length_in);
 }
-void Sub(double* vector_inout, double* vector_in, unsigned length_in, cudaStream_t stream) {
+void MathGPU::Sub(double *vector_inout, double *vector_in, unsigned length_in, cudaStream_t stream)
+{
     unsigned length = MathGPU::properties->maxThreadsPerBlock;
     dim3 T(length);
-    dim3 B((length_in+T.x-1u)/T.x);
-    _Sub<<<T,B,0,stream>>>(vector_inout, vector_in, length_in);
+    dim3 B((length_in + T.x - 1u) / T.x);
+    _Sub<<<T, B, 0, stream>>>(vector_inout, vector_in, length_in);
 }
-void Mul(double* vector_inout, double* vector_in, unsigned length_in, cudaStream_t stream) {
+void MathGPU::Mul(double *vector_inout, double *vector_in, unsigned length_in, cudaStream_t stream)
+{
     unsigned length = MathGPU::properties->maxThreadsPerBlock;
     dim3 T(length);
-    dim3 B((length_in+T.x-1u)/T.x);
-    _Mul<<<T,B,0,stream>>>(vector_inout, vector_in, length_in);
+    dim3 B((length_in + T.x - 1u) / T.x);
+    _Mul<<<T, B, 0, stream>>>(vector_inout, vector_in, length_in);
 }
-void Mul(double* vector_inout, double value_in, unsigned length_in, cudaStream_t stream) {
+void MathGPU::Mul(double *vector_inout, double value_in, unsigned length_in, cudaStream_t stream)
+{
     unsigned length = MathGPU::properties->maxThreadsPerBlock;
     dim3 T(length);
-    dim3 B((length_in+T.x-1u)/T.x);
-    _Mul<<<T,B,0,stream>>>(vector_inout, value_in, length_in);
+    dim3 B((length_in + T.x - 1u) / T.x);
+    _Mul<<<T, B, 0, stream>>>(vector_inout, value_in, length_in);
 }
+
+cublasOperation_t MapStructure(MatrixStructure structure)
+{
+    cublasOperation_t operation;
+    switch (structure)
+    {
+    case MatrixStructure::Natural:
+        operation = cublasOperation_t::CUBLAS_OP_N;
+        break;
+    case MatrixStructure::Transposed:
+        operation = cublasOperation_t::CUBLAS_OP_T;
+        break;
+    default:
+        std::cout << "Structure not defined for the left matrix. Using default structure.\n";
+        return CUBLAS_OP_N;
+        break;
+    }
+    return operation;
+}
+
+// Matrix Multiplication
+void MathGPU::MatrixMultiplication(double *matrix_out, double alpha, double beta,
+                                   double *matrixLeft_in, MatrixStructure structureLeft, unsigned lengthLeftX_in, unsigned lengthLeftY_in,
+                                   double *matrixRight_in, MatrixStructure structureRight, unsigned lengthRightX_in, unsigned lengthRightY_in,
+                                   double *weight_in, cudaStream_t stream)
+{
+    cublasHandle_t handle;
+    cublasOperation_t left_OP, right_OP;
+    cublasCreate_v2(&handle);
+    cublasSetStream_v2(handle, stream);
+    cublasOperation_t left_OP = MapStructure(structureLeft);
+    cublasOperation_t right_OP = MapStructure(structureRight);
+    if (lengthLeftY_in != lengthRightX_in)
+    {
+        std::cout << "Error: multiplication sizes do not match.\n";
+        return;
+    }
+    cublasDgemm_v2(handle, left_OP, right_OP, lengthLeftX_in, lengthRightY_in, lengthLeftY_in, &alpha, matrixLeft_in, lengthLeftX_in, matrixRight_in, lengthRightX_in, &beta, matrix_out, lengthLeftX_in);
+    cublasDestroy_v2(handle);
+}
+
+// Operators
+void MathGPU::Operation(void (*operation_in)(double *matrix_out, double *matrixLeft_in, double *matrixRight_in, unsigned length_in),
+                        double *matrix_out, double *matrixLeft_in, double *matrixRight_in, unsigned length_in,
+                        unsigned strideOutput_in, unsigned strideLeft_in, unsigned strideRight_in, unsigned iteration_in, unsigned offsetOutput_in, unsigned offsetLeft_in, unsigned offsetRight_in)
+{
+    double *output = matrix_out + offsetOutput_in;
+    double *left = matrixLeft_in + offsetLeft_in;
+    double *right = matrixRight_in + offsetRight_in;
+    if (length_in < strideLeft_in)
+    {
+        std::cout << "Warning: The behavior is undefined to output strides lower than the length of the operation.\n";
+    }
+    for (unsigned i = 0u; i < iteration_in; i++)
+    {
+        operation_in(output + i * strideOutput_in, left + i * strideLeft_in, right + i * strideRight_in, length_in);
+    }
+}
+void MathGPU::Operation(void (*operation_in)(double *matrixLeft_inout, double *matrixRight_in, unsigned length_in),
+                        double *matrixLeft_inout, double *matrixRight_in, unsigned length_in,
+                        unsigned strideLeft_in, unsigned strideRight_in, unsigned iteration_in, unsigned offsetLeft_in, unsigned offsetRight_in)
+{
+    double *left = matrixLeft_inout + offsetLeft_in;
+    double *right = matrixRight_in + offsetRight_in;
+    if (length_in < strideLeft_in)
+    {
+        std::cout << "Warning: The behavior is undefined to left strides lower than the length of the operation.\n";
+    }
+    for (unsigned i = 0u; i < iteration_in; i++)
+    {
+        operation_in(left + i * strideLeft_in, right + i * strideRight_in, length_in);
+    }
+}
+
+// Reducibles Operations
+double Mean(double *vector_in, unsigned length_in, double *weight_in = NULL);
+void Mean(double *vector_out, double *matrix_in, unsigned lengthX_in, unsigned lengthY_in, double *weight_in = NULL);
